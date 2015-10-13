50b210e9b48fa5a4bf9ea22ac0242ad3f7bc2ba0
Barnabas Bulpett <bbulpett@charter.net>
2015-10-12 22:45:11 -0400

    Set up broadcasting and subscriptions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,10 @@ class MessagesController < ApplicationController
   end
 
   def create
-
+  	ActionCable.server.broadcast 'messages',
+  		message: params[:message][:body],
+  		username: cookies.signed[:username]
+  		
   	# respond with a success status code for every request
   	head :ok
   end
