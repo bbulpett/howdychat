99413c3601e146e73d08504412dcab2df9481d01
Barnabas Bulpett <bbulpett@charter.net>
2015-10-13 22:26:36 -0400

    Added Message model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-
-  get 'messages/index'
-
-  get 'messages/create'
-
-  get 'pages/home'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :messages, only: [:index, :create]
   resources :sessions, only: [:new, :create]
